401edcde2be442fce533e3a9d35e7aee83cb86cd
SongHae <thdgo456@gmail.com>
2022-08-19 21:29:36 +0900

    중복을 제거 하기 위해 ConnectionMaker 메서드로 분리

--- src/main/java/com/spring/user/DaoFactory.java
+++ src/main/java/com/spring/user/DaoFactory.java
@@ -1,15 +1,22 @@
 package com.spring.user;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class DaoFactory {
 
+    @Bean
     public UserDao userDao(){
-        return new UserDao(new ConnectionMakerForA());
+        return new UserDao(getConnectorMaker());
     }
 
+    @Bean
     public AccountDao accountDao(){
-        return new AccountDao(new ConnectionMakerForA());
+        return new AccountDao(getConnectorMaker());
     }
 
+    @Bean
     public MessageDao messageDao(){
         return new MessageDao(getConnectorMaker());
     }
